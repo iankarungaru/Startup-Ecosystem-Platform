8dc682bf89960eac66300f1dde6eb41d105b1527
Sylvester976 <officialotienosylvester@gmail.com>
2025-03-19 14:47:01 +0300

    added mysql

--- govtech/landingPage/urls.py
+++ govtech/landingPage/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),  
+    path('authlogin/', views.authlogin, name = 'authlogin')
 ]
