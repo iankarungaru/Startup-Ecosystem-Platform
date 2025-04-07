8f92213ddee540cb4e2e8d0e308a92c8e0cce2ba
Sylvester976 <officialotienosylvester@gmail.com>
2025-04-07 11:32:47 +0300

    resolved that landing page custom user error

--- govtech/landingPage/views.py
+++ govtech/landingPage/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from validate_email_address import validate_email
 from django.contrib.auth.models import User  # Import User model
 from django.contrib.auth import authenticate, login
-from landingPage.models import CustomUser  # Import your custom model  # Import authenticate and login
+# from landingPage.models import CustomUser  # Import your custom model  # Import authenticate and login
 
 # Create your views here.
 def landing(request):
@@ -15,7 +15,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
-from landingPage.models import CustomUser
+# from landingPage.models import CustomUser
 
 def register_view(request):
     if request.method == 'POST':
