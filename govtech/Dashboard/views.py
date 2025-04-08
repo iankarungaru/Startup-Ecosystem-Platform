6a57dc04fa5297ca4b0bf9e17147feada2e186ce
Edan Odongo <edanodongo05@gmail.com>
2025-04-08 20:54:32 +0300

    Updated Dashboard

--- govtech/Dashboard/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-def dashboard(request):
-    return render(request,"dashboard.html")
-
-def index(request):
-    return render(request,"index.html")
--- govtech/startup/settings.py
+++ govtech/startup/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'startupRegistration',
-    'Dashboard',
     'debug_toolbar',
     'landingPage',   
 ]
