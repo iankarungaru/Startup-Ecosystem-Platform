6ca203bfb5b341b4d5a01ae4a09a1620df1682e6
Edan Odongo <edanodongo05@gmail.com>
2025-04-09 09:57:46 +0300

    Updated dashboard

--- govtech/startup/settings.py
+++ govtech/startup/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'startupRegistration',
+    'dashboard',
     'debug_toolbar',
     'landingPage',   
 ]
