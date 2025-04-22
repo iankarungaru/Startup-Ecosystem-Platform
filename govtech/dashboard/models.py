e00461ebb8583fb6bcf38f53dce99152719b43e6
Edan Odongo <edanodongo05@gmail.com>
2025-04-22 10:31:46 +0300

    Update registration details

--- govtech/dashboard/models.py
+++ govtech/dashboard/models.py
@@ -8,23 +8,40 @@ from django.db import models
 
 class Registration(models.Model):
     # Step 1: Basic Company Information
-    startup_name = models.CharField(max_length=255)
-    stage = models.CharField(max_length=50, choices=[('Idea', 'Idea'), ('MVP', 'MVP'), ('Early-stage', 'Early-stage'), ('Growth', 'Growth'), ('Scaling', 'Scaling')])
+    company_name = models.CharField(max_length=255)
+    stage = models.CharField(max_length=50, choices=[
+        ('Idea', 'Idea'),
+        ('MVP', 'MVP'),
+        ('Early-stage', 'Early-stage'),
+        ('Growth', 'Growth'),
+        ('Scaling', 'Scaling')
+        ], default='Idea')
     official_email = models.EmailField()
     phone_number = models.CharField(max_length=20)
+    registration_number = models.CharField(max_length=255, default='CPR/201x/xxxxx')
+    contact_person = models.CharField(max_length=255, default='John Doe')
     date_of_establishment = models.DateField()
     physical_address = models.TextField()
     
     # Step 2: Business Details
     tax_identification_number = models.CharField(max_length=100)
     employees = models.IntegerField()
-    startup_description = models.TextField()
+    company_description = models.TextField()
+    nature = models.CharField(max_length=50, choices=[
+        ('Software Development', 'Software Development'),
+        ('Web Services', 'Web Services'),
+        ('Mobile Apps', 'Mobile Apps'),
+        ('Desktop Apps', 'Desktop Apps'),
+        ('IoT Apps', 'IoT Apps')
+        ], default='Software Development')
+    specialization = models.CharField(max_length=255, default='Web Development')
     sector = models.CharField(max_length=100)
     business_model = models.CharField(max_length=50, choices=[('B2B', 'B2B'), ('B2C', 'B2C'), ('SaaS', 'SaaS'), ('Marketplace', 'Marketplace')])
     website = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return self.startup_name
+        return self.company_name
+    
 from django.db import models
 
 class IndividualDev(models.Model):
@@ -36,6 +53,13 @@ class IndividualDev(models.Model):
     contact = models.CharField(max_length=20)
     industry = models.CharField(max_length=100)
     address = models.TextField()
+    nature = models.CharField(max_length=50, choices=[
+        ('Software Development', 'Software Development'),
+        ('Web Services', 'Web Services'),
+        ('Mobile Apps', 'Mobile Apps'),
+        ('Desktop Apps', 'Desktop Apps'),
+        ('IoT Apps', 'IoT Apps')
+        ], default='Software Development')
     description = models.TextField()
 
     def __str__(self):
