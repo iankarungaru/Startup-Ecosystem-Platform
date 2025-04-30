e219ef7de95499435266a2d9f80de7d74caad59a
Sylvester976 <officialotienosylvester@gmail.com>
2025-04-30 12:10:44 +0300

    Assets being added

--- govtech/systemAdmin/views.py
+++ govtech/systemAdmin/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     # Explicitly specify the path to the template for sysadmin
-    return render(request, 'sysadmin/index.html')
+    return render(request, 'sysadmin/admin.html')
