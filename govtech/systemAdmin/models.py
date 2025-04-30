ac79e2702142eeb42ccb41f5d50d4828ec36c7da
Sylvester976 <officialotienosylvester@gmail.com>
2025-04-30 13:42:09 +0300

    We can login to sysadmin

--- govtech/systemAdmin/models.py
+++ govtech/systemAdmin/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 class InternalUser(models.Model):
     fName = models.CharField(max_length=255)
@@ -24,3 +26,33 @@ class InternalUser(models.Model):
 
     def __str__(self):
         return f"{self.fName} {self.lName}"
+
+class LoginAttempt(models.Model):
+    email = models.EmailField()
+    ip_address = models.GenericIPAddressField()
+    attempts = models.IntegerField(default=0)
+    last_attempt = models.DateTimeField(auto_now=True)
+
+    def is_locked_out(self):
+        # You can lockout after 5 attempts within 10 minutes, for example
+        return self.attempts >= 5 and timezone.now() - self.last_attempt < timedelta(minutes=10)
+
+    def reset_if_expired(self):
+        if timezone.now() - self.last_attempt > timedelta(minutes=10):
+            self.attempts = 0
+            self.save()
+
+class PasswordResetToken(models.Model):
+    user = models.ForeignKey('InternalUser', on_delete=models.CASCADE)
+    token = models.CharField(max_length=8)  # OTPs are 8-char, alphanumeric
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_valid(self):
+        # Valid for 10 minutes
+        return timezone.now() < self.created_at + timedelta(minutes=10)
+
+    class Meta:
+        db_table = 'password_reset_token'
+
+    def __str__(self):
+        return f'Token for {self.user.email}'
--- govtech/systemAdmin/urls.py
+++ govtech/systemAdmin/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='login'),
+    path('', views.index, name='sysadmin'),
+    path('authorize_login/', views.authorize_login, name='authorize_login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 
 ]
